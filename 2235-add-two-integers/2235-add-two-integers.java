5fd36534fb52b41fc9725eb10a5cc8d00403d12c
Ramavath Meghana <ramavathmeghana08@gmail.com>
2026-07-05 23:53:50 +0530

    Time: 0 ms (100%), Space: 42.2 MB (74.11%) - LeetHub

---
+++ 2235-add-two-integers/2235-add-two-integers.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int sum(int num1, int num2) {
+       int sum = num1 +num2;
+       if(num1>num2)
+       {
+        return sum;
+       }
+       else 
+       {
+        return  sum;
+       }
+    }
+}
